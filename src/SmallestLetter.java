51202e013a594cd44073a6e77f52c520360fcb14
Ibrahim Anas <anasakd924@gmail.com>
2025-12-20 14:17:06 +0530

    Smallest letter greater than target.

---
+++ src/SmallestLetter.java
@@ -0,0 +1,25 @@
+//LEETCODE 744. Find Smallest letter greater than target.
+
+class Solution {
+
+    public static void main(String []args){
+        char[] letters = {'c','f','j'};
+        char target= 'a';
+        char result = nextGreatestLetter(letters, target);
+        System.out.println("Letter found, Value is : " + result);
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length-1;
+
+        while(left<=right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return letters[left % letters.length];
+    }
+}
